b78d41727ce1b3f4e83cc40190752702a5fd4506
Roger Han <rogerhan1228@gmail.com>
2026-09-21 16:01:18 -0400

    remove no need files

--- robot_config.py
+++ robot_config.py
@@ -77,6 +77,9 @@ TURN_SPEED = 700
 TURN_ACCELERATION = 700
 
 # Set up the drive base.
+#Roger's robot uses a 56 mm wheel diameter and a 114 mm axle track.
+#DL:D, DR:B, RA:A, LA:E, LC:C, RC:F
+#You can change the ports to match your robot configuration.
 DRIVE_LEFT = Motor(Port.D, Direction.CLOCKWISE)
 DRIVE_RIGHT = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 DRIVE_BASE = DriveBase(DRIVE_LEFT, DRIVE_RIGHT, 56, 114)
